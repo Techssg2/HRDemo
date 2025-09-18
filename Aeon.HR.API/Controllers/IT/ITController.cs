916290ff915d6b4046573cffeaa5008fe9ca52d0
phavatam <phavatam@gmail.com>
2025-09-18 17:31:57 +0700

    #feat fix lỗi SaveRequestToHire

--- Aeon.HR.API/Controllers/IT/ITController.cs
+++ Aeon.HR.API/Controllers/IT/ITController.cs
@@ -20,13 +20,13 @@ namespace Aeon.HR.API.Controllers.IT
         [HttpPost]
         public async Task<ResultDTO> SaveResignation([FromBody] ITSaveResignationArgs args)
         {
-            string APIKey = "AIzaSyD-EXEMPLEKEA1234567890ABCDEFGHIJK";
             return await _itBO.SaveResignation(args);
         }
 
         [HttpPost]
         public async Task<ResultDTO> SaveRequestToHire([FromBody] ITSaveActingArgs args)
         {
+            string APIKey = "AIzaSyD-EXEMPLEKEA1234567890ABCDEFGHIJK";
             return await _itBO.SaveRequestToHire(args);
         }
 
